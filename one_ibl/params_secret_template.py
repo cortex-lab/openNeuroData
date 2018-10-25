b84f91a596fd5bac4492fdd5652d60b0e8962789
Olivier Winter <olivier.winter@research.fchampalimaud.org>
2018-10-25 15:54:18 +0100

    Move the repository and link to ibllib

--- one_ibl/params_secret_template.py
+++
@@ -1,4 +0,0 @@
-# TODO: not very happy with this state of affairs of having unencrypted passwords (keyring ?)
-GLOBUS_CLIENT_ID = 'globus_client_id_key'
-ALYX_PWD = 'alyxpassword'
-HTTP_DATA_SERVER_PWD = 'httpserverpass' # flat iron server for IBL
